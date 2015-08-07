a90bde07dc1e07db1c140c3d94fc1d5b51996e11
Wes Morgan <cap10morgan@gmail.com>
2015-08-07 16:32:43 -0600

    [WM] update deps; remove pedestal-tb snapshot

--- resources/leiningen/new/kraken_http_api/project.clj
+++ resources/leiningen/new/kraken_http_api/project.clj
@@ -1,18 +1,18 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
-  :description ""
+  :description "FIXME: HTTP API gateway for ..."
   :url "https://github.com/democracyworks/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.2.0"]
-                 [com.novemberain/langohr "3.2.0"]
+                 [com.novemberain/langohr "3.3.0"]
                  [prismatic/schema "0.4.3"]
                  [ch.qos.logback/logback-classic "1.1.3"]
 
                  [io.pedestal/pedestal.service "0.4.0"]
                  [io.pedestal/pedestal.service-tools "0.4.0"]
-                 [democracyworks/pedestal-toolbox "0.6.1-SNAPSHOT"]
+                 [democracyworks/pedestal-toolbox "0.6.1"]
 
                  ;; this has to go before pedestal.immutant
                  ;; until this is fixed:
