1507a8b9e8f5dae160e1cfac21cc414b6f09bee1
Wes Morgan <cap10morgan@gmail.com>
2015-08-07 16:33:28 -0600

    [WM] update queue to kehaar 0.5.0

--- resources/leiningen/new/kraken_http_api/queue.clj
+++ resources/leiningen/new/kraken_http_api/queue.clj
@@ -6,40 +6,28 @@
             [langohr.queue :as lq]
             [kehaar.core :as k]
             [kehaar.wire-up :as wire-up]
+            [kehaar.rabbitmq]
             [{{name}}.channels :as channels]
-            [{{name}}.handlers :as handlers]
             [turbovote.resource-config :refer [config]]))
 
 (defn initialize []
-  (let [connection (atom nil)
-        max-retries 5]
-    (loop [attempt 1]
-      (try
-        (reset! connection
-                (rmq/connect (or (config :rabbitmq :connection)
-                                 {})))
-        (log/info "RabbitMQ connected.")
-        (catch Throwable t
-          (log/warn "RabbitMQ not available:" (.getMessage t) "attempt:" attempt)))
-      (when (nil? @connection)
-        (if (< attempt max-retries)
-          (do (Thread/sleep (* attempt 1000))
-              (recur (inc attempt)))
-          (do (log/error "Connecting to RabbitMQ failed. Bailing.")
-              (throw (ex-info "Connecting to RabbitMQ failed" {:attempts attempt}))))))
-    (let [ins []
-          in-outs [(wire-up/incoming-service-handler @connection
-                                                     "{{name}}.ok"
-                                                     (config :rabbitmq :queues "{{name}}.ok")
-                                                     handlers/ok)]
-          out-ins []
-          outs []]
-      {:connections #{@connection}
-       :channels (concat
-                  ins
-                  in-outs
-                  out-ints
-                  outs)})))
+  (let [max-retries 5
+        connection (kehaar.rabbitmq/connect-with-retries max-retries)]
+    (let [incoming-events []
+          incoming-services [(wire-up/incoming-service
+                              connection
+                              "{{name}}.ok"
+                              (config [:rabbitmq :queues "{{name}}.ok"])
+                              channels/ok-requests
+                              channels/ok-responses)]
+          external-services []
+          outgoing-events []]
+      {:connections [connection]
+       :channels (vec (concat
+                       incoming-events
+                       incoming-services
+                       external-services
+                       outgoing-events))})))
 
 (defn close-resources! [resources]
   (doseq [resource resources]
