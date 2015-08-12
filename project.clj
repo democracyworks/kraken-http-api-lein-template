ecc0e9a8659c12a4016b2494239e7aada9364330
Wes Morgan <cap10morgan@gmail.com>
2015-08-12 13:22:08 -0600

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kraken-http-api/lein-template "0.1.0-SNAPSHOT"
+(defproject kraken-http-api/lein-template "0.1.0"
   :description "Leiningen template for kraken-http-api components"
   :url "https://github.com/democracyworks/kraken-http-api-lein-template"
   :license {:name "Eclipse Public License"
