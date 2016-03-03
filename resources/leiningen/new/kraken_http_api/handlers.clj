a047bdf13878a5ecf29c0dd06a6ace2afa9ce666
Eric Normand <ericwnormand@gmail.com>
2016-03-03 09:29:31 -0600

    [EN] Remove handlers.clj from template.

--- resources/leiningen/new/kraken_http_api/handlers.clj
+++
@@ -1,8 +0,0 @@
-(ns {{name}}.handlers
-  (:require [clojure.tools.logging :as log]))
-
-(defn ok
-  "A handler that does nothing and responds ok."
-  [message]
-  (log/info "Received:" message)
-  {:status :ok})
