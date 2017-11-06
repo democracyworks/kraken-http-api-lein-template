3876d6db4ba2bed146b0d3bda28781131e9c9f18
Sean Duckett <sduckett@gmail.com>
2017-11-06 15:45:24 -0700

    Update dependencies, go toward clojure 1.9.

--- resources/leiningen/new/kraken_http_api/project.clj
+++ resources/leiningen/new/kraken_http_api/project.clj
@@ -3,29 +3,29 @@
   :url "https://github.com/democracyworks/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [turbovote.resource-config "0.2.1"]
-                 [com.novemberain/langohr "3.7.0"]
-                 [prismatic/schema "1.1.6"]
-                 [ch.qos.logback/logback-classic "1.2.2"]
-                 [democracyworks/kehaar "0.10.4"]
-                 [io.pedestal/pedestal.service "0.5.2"]
-                 [io.pedestal/pedestal.service-tools "0.5.2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [democracyworks/bifrost "0.2.0"]
+                 [democracyworks/kehaar "0.11.2"]
                  [democracyworks/pedestal-toolbox "0.7.0"]
-                 [org.immutant/web "2.1.6"]
-                 [io.pedestal/pedestal.immutant "0.5.2"]
-                 [org.immutant/core "2.1.6"]
-                 [democracyworks/bifrost "0.1.5"]]
+                 [io.pedestal/pedestal.immutant "0.5.3"]
+                 [io.pedestal/pedestal.log "0.5.3"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [io.pedestal/pedestal.service-tools "0.5.3"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.immutant/core "2.1.9"]
+                 [org.immutant/web "2.1.9"]
+                 [turbovote.resource-config "0.2.1"]]
   :plugins [[lein-immutant "2.1.0"]
             [com.pupeno/jar-copier "0.4.0"]]
-  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.35.1"]]
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.43.0"]]
   :jar-copier {:java-agents true
                :destination "resources/jars"}
   :prep-tasks ["javac" "compile" "jar-copier"]
   :main ^:skip-aot {{name}}.server
   :uberjar-name "{{name}}.jar"
   :profiles {:uberjar {:aot :all}
-             :dev {:resource-paths ["dev-resources"]}
-             :test {:dependencies [[clj-http "3.5.0"]]
+             :dev {:resource-paths ["dev-resources"]
+                   :jvm-opts ["-Dlog-level=DEBUG"]}
+             :test {:dependencies [[clj-http "3.7.0"]]
                     :jvm-opts ["-Dlog-level=INFO"]}})
